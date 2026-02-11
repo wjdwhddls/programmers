2e2cdb7e71428d8fc1fa793c4a792b0b7ff30728
jjong0425 <bfpark99@naver.com>
2026-02-11 15:47:46 +0900

    0210 practice

---
+++ 코테 준비/2월/PCCE_5.java
@@ -0,0 +1,16 @@
+public class PCCE_5 {
+    public int[] solution(String[] cpr) {
+        int[] answer = {0, 0, 0, 0, 0};
+        String[] basic_order = {"check", "call", "pressure", "respiration", "repeat"};
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < basic_order.length; j++) {
+                if (cpr[i].equals(basic_order[j])) {
+                    answer[i] = j + 1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
